fe34d3d2437cc157162da121fcf7aec36bc4c925
Megatron02 <gadzova14@mail.ru>
2017-06-05 21:15:08 +0300

    Создан проект

---
+++ Chess_for_three/Chess_for_three/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Chess_for_three
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Chess_for_three/Chess_for_three/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chess_for_three.MvcApplication" Language="C#" %>
